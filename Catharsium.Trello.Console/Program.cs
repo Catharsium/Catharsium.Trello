59bf87284df0114d991c390347389ebf43dc0c5c
Thierry Brachthuizer <thierry.brachthuizer@unifiedpost.com>
2019-10-21 08:03:35 +0200

    Add console app and initial reading of json

---
+++ Catharsium.Trello.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Catharsium.Trello.Core;
+
+namespace Catharsium.Trello.Console
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            var board = await new Downloader().Json();
+            System.Console.WriteLine(board.Name);
+        }
+    }
+}
