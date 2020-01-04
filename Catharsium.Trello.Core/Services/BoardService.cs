c1d59c16c0e09b1d473c58e6bab69d5de8bbbeb7
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2020-01-04 09:43:42 +0100

    Add Catharsium.Trello.Data project

---
+++ Catharsium.Trello.Core/Services/BoardService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catharsium.Trello.Core.Services
+{
+    class BoardService
+    {
+    }
+}
---
+++ Catharsium.Trello.Data/_Configuration/TrelloDataRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Catharsium.Util.Configuration.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catharsium.Trello.Core._Configuration
+{
+    public static class TrelloDataRegistration
+    {
+        public static IServiceCollection AddTrelloData(this IServiceCollection services, IConfiguration configuration)
+        {
+            var trelloCoreConfiguration = configuration.Load<TrelloDataConfiguration>();
+            services.AddSingleton<TrelloDataConfiguration, TrelloDataConfiguration>(provider => trelloCoreConfiguration);
+
+            return services;
+        }
+    }
+}
