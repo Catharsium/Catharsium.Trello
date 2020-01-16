b636d5dcadbfe1c6c8f1262ec41780c51beec57b
Thierry Brachthuizer <thierry.brachthuizer@unifiedpost.com>
2020-01-16 11:54:51 +0100

    Cleanup DateFilterTests

--- Catharsium.Trello.Core.Tests/Filters/DateFilterTests.cs
+++ Catharsium.Trello.Core.Tests/Filters/DateFilterTests.cs
@@ -1,10 +1,10 @@
 ﻿using Catharsium.Trello.Core.Filters;
 using Catharsium.Trello.Models;
+using Catharsium.Trello.Models.Interfaces.Core;
 using Catharsium.Util.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using Catharsium.Trello.Models.Interfaces.Core;
 using NSubstitute;
+using System;
 
 namespace Catharsium.Trello.Core.Tests.Filters
 {
@@ -13,21 +13,29 @@ namespace Catharsium.Trello.Core.Tests.Filters
     {
         #region Fixture
 
+        private Card Card { get; set; }
+
+
+        [TestInitialize]
+        public void SetupProperties()
+        {
+            this.Card = new Card {Id = "My id"};
+        }
 
         #endregion
 
+        #region Includes
 
         [TestMethod]
         public void Includes_CreationDateBeforeFromDate_ReturnsFalse()
         {
-            var card = new Card {Id = "My id"};
             var fromDate = DateTime.Now.AddDays(-1);
             var toDate = DateTime.Now.AddDays(1);
-            this.GetDependency<ICreationDateRetriever>().FindCreationDate(card.Id).Returns(fromDate.AddDays(-1));
+            this.GetDependency<ICreationDateRetriever>().FindCreationDate(this.Card.Id).Returns(fromDate.AddDays(-1));
             this.SetDependency(fromDate, "fromDate");
             this.SetDependency(toDate, "toDate");
 
-            var actual = this.Target.Includes(card);
+            var actual = this.Target.Includes(this.Card);
             Assert.IsFalse(actual);
         }
 
@@ -35,14 +43,13 @@ namespace Catharsium.Trello.Core.Tests.Filters
         [TestMethod]
         public void Includes_CreationDateEqualToFromDate_ReturnsTrue()
         {
-            var card = new Card {Id = "My id"};
             var fromDate = DateTime.Now.AddDays(-1);
             var toDate = DateTime.Now.AddDays(1);
-            this.GetDependency<ICreationDateRetriever>().FindCreationDate(card.Id).Returns(fromDate);
+            this.GetDependency<ICreationDateRetriever>().FindCreationDate(this.Card.Id).Returns(fromDate);
             this.SetDependency(fromDate, "fromDate");
             this.SetDependency(toDate, "toDate");
 
-            var actual = this.Target.Includes(card);
+            var actual = this.Target.Includes(this.Card);
             Assert.IsTrue(actual);
         }
 
@@ -50,14 +57,13 @@ namespace Catharsium.Trello.Core.Tests.Filters
         [TestMethod]
         public void Includes_CreationDateBetweenFromAndToDate_ReturnsTrue()
         {
-            var card = new Card {Id = "My id"};
             var fromDate = DateTime.Now.AddDays(-1);
             var toDate = DateTime.Now.AddDays(1);
-            this.GetDependency<ICreationDateRetriever>().FindCreationDate(card.Id).Returns(fromDate.AddDays(1));
+            this.GetDependency<ICreationDateRetriever>().FindCreationDate(this.Card.Id).Returns(fromDate.AddDays(1));
             this.SetDependency(fromDate, "fromDate");
             this.SetDependency(toDate, "toDate");
 
-            var actual = this.Target.Includes(card);
+            var actual = this.Target.Includes(this.Card);
             Assert.IsTrue(actual);
         }
 
@@ -65,14 +71,13 @@ namespace Catharsium.Trello.Core.Tests.Filters
         [TestMethod]
         public void Includes_CreationDateEqualToToDate_ReturnsFalse()
         {
-            var card = new Card {Id = "My id"};
             var fromDate = DateTime.Now.AddDays(-1);
             var toDate = DateTime.Now.AddDays(1);
-            this.GetDependency<ICreationDateRetriever>().FindCreationDate(card.Id).Returns(toDate);
+            this.GetDependency<ICreationDateRetriever>().FindCreationDate(this.Card.Id).Returns(toDate);
             this.SetDependency(fromDate, "fromDate");
             this.SetDependency(toDate, "toDate");
 
-            var actual = this.Target.Includes(card);
+            var actual = this.Target.Includes(this.Card);
             Assert.IsTrue(actual);
         }
 
@@ -80,15 +85,16 @@ namespace Catharsium.Trello.Core.Tests.Filters
         [TestMethod]
         public void Includes_CreationDateLargerThanToDate_ReturnsFalse()
         {
-            var card = new Card {Id = "My id"};
             var fromDate = DateTime.Now.AddDays(-1);
             var toDate = DateTime.Now.AddDays(1);
-            this.GetDependency<ICreationDateRetriever>().FindCreationDate(card.Id).Returns(toDate.AddDays(1));
+            this.GetDependency<ICreationDateRetriever>().FindCreationDate(this.Card.Id).Returns(toDate.AddDays(1));
             this.SetDependency(fromDate, "fromDate");
             this.SetDependency(toDate, "toDate");
 
-            var actual = this.Target.Includes(card);
+            var actual = this.Target.Includes(this.Card);
             Assert.IsFalse(actual);
         }
+
+        #endregion
     }
 }
